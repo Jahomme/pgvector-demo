21d05ec91ebfb922bd9cd2a1b0b5b2593ba09621
Jadilson <jadilsonigor@gmail.com>
2026-06-27 13:39:21 -0300

    remove /dist

--- dist/src/db/index.d.ts
+++
@@ -1,6 +0,0 @@
-import postgres from 'postgres';
-import * as schema from './schema';
-import 'dotenv/config';
-export declare const db: import("drizzle-orm/postgres-js").PostgresJsDatabase<typeof schema> & {
-    $client: postgres.Sql<{}>;
-};
--- dist/src/documents/dto/documents.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class IngestDto {
-    title: string;
-    content: string;
-}
-export declare class SearchDto {
-    query: string;
-}
