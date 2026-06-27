b33564356205332b6c8da811dde8becc00e84865
Jadilson <jadilsonigor@gmail.com>
2026-06-27 13:37:22 -0300

    feat: tratamento global de exceções

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,8 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
+const http_exception_filter_1 = require("./common/filters/http-exception.filter");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
     const config = new swagger_1.DocumentBuilder()
         .setTitle('pgvector Demo API')
         .setDescription('API para demonstração de IA Generativa e busca vetorial com pgvector')
